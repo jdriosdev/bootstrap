da15646ffdd97e208c19f64fd6252a155ee33ff0
Juan Rios <mrdaniel19996@gmail.com>
2020-09-28 10:39:01 -0500

    [ADD]todo app bootstrap and react

---
+++ src/components/TodoAdd.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useForm } from '../hooks/useForm';
+
+export const TodoAdd = ({handleAddTodo}) => {
+
+  const [{description}, handleInputChange, reset] = useForm({
+    description: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if( description.trim().length <= 1) {
+      return
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+
+    
+    handleAddTodo( newTodo )
+    reset()
+  }
+
+  return (
+    <>
+      <h4> Add TODO</h4>
+          <hr />
+
+          <form 
+            onSubmit={handleSubmit}
+          >
+            <input
+              type='text'  
+              name='description'
+              className='form-control'
+              placeholder='Your task...'
+              autoComplete='off'
+              value={description}
+              onChange={handleInputChange}
+            />
+
+            <button 
+              type='submit'
+              className='btn btn-outline-dark mt-1 btn-block'>
+              Add New
+            </button>
+            
+          </form>
+    </>
+  )
+}
