76b6a2ba38e17136387feaafc4c933c18d4fb816
Juan Rios <mrdaniel19996@gmail.com>
2020-09-28 11:11:28 -0500

    [UPDATE]css fix build

---
+++ docs/precache-manifest.dfcc160aa815bf55a2f223c677f5d134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7b4d3e582d65c67e583ace7c2a568f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca73fe71dd2e8d2c5c87",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5564af73baea2e93c878",
+    "url": "/static/css/main.c2b28c29.chunk.css"
+  },
+  {
+    "revision": "ca73fe71dd2e8d2c5c87",
+    "url": "/static/js/2.07adcba7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07adcba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5564af73baea2e93c878",
+    "url": "/static/js/main.2eae9f06.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
